2ab2c1090a8eed6c11fbb791a14a02b4bbeb794c
Baizx98 <baizx98@gmail.com>
2025-02-28 20:44:07 +0800

    [core] 修复了单词分隔和输出的bug，更新了默认词库

--- wordtools.py
+++ wordtools.py
@@ -1,4 +1,6 @@
-def process_vocabulary(variant_file, cet6_file, output_file, notfound_file):
+def process_vocabulary(
+    variant_file, cet6_file, output_file, notfound_file, debug=False
+):
     """
     处理词汇表，生成包含原始单词及其变体的输出文件，以及未找到的单词列表。
 
@@ -41,21 +43,23 @@ def process_vocabulary(variant_file, cet6_file, output_file, notfound_file):
         for word in cet6_words:
             if word in variant_map:
                 variants = variant_map[word]
-                f_out.write(",".join(variants) + "\n")
+                for variant in variants:
+                    f_out.write(variant + "\n")
             else:
                 f_out.write(word + "\n")
                 not_found_words.append(word)
 
-    # 将未找到的单词写入文件
-    with open(notfound_file, "w", encoding="utf-8") as f_notfound:
-        for word in not_found_words:
-            f_notfound.write(word + "\n")
+    if debug:
+        # 将未找到的单词写入文件
+        with open(notfound_file, "w", encoding="utf-8") as f_notfound:
+            for word in not_found_words:
+                f_notfound.write(word + "\n")
 
 
 if __name__ == "__main__":
     # 示例用法
-    variant_file = "./ECDICT/lemma.en.txt"  # 包含单词变体信息的文件
-    cet6_file = "./cet6word.txt"  # 包含CET6词汇表的文件
+    variant_file = "./vocab/lemma.en.txt"  # 包含单词变体信息的文件
+    cet6_file = "./vocab/CET6_vocab_20250228.txt"  # 包含CET6词汇表的文件
     output_file = "output_words.txt"  # 输出文件
     notfound_file = "notfound.txt"  # 未找到的单词输出文件
 
